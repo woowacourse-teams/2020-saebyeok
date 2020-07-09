56ce30530281a0d6d683fd98b5ce72119e0784d3
sujin <sujinemilycho@gmail.com>
2020-07-09 11:19:06 +0900

    init

---
+++ src/main/java/com/saebyeok/saebyeok/SaebyeokApplication.java
@@ -0,0 +1,13 @@
+package com.saebyeok.saebyeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaebyeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaebyeokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saebyeok/saebyeok/SaebyeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saebyeok.saebyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaebyeokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
