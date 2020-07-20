6e609b101e511c23eb1c5cc43f8932726b009536
icyMojito <dev.icymojito@gmail.com>
2020-07-20 16:09:15 +0900

    refactor: 예외를 처리하는 ControllerAdvice 클래스의 이름 및 반환값 변경

--- src/main/java/com/saebyeok/saebyeok/controller/GlobalExceptionAdvice.java
+++ src/main/java/com/saebyeok/saebyeok/controller/GlobalExceptionAdvice.java
@@ -4,18 +4,21 @@ import com.saebyeok.saebyeok.dto.ExceptionResponse;
 import com.saebyeok.saebyeok.exception.CommentNotFoundException;
 import com.saebyeok.saebyeok.exception.InvalidLengthCommentException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionController {
+public class GlobalExceptionAdvice {
     // TODO: 2020/07/20 exception을 어떻게 응답 처리할 것인지 방법을 고민해보자
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({InvalidLengthCommentException.class, CommentNotFoundException.class})
-    public ExceptionResponse validated(RuntimeException e) {
-        return new ExceptionResponse(e.getMessage());
+    public ResponseEntity<ExceptionResponse> validated(RuntimeException e) {
+        return ResponseEntity.
+            badRequest().
+            body(new ExceptionResponse(e.getMessage()));
     }
 }
 
