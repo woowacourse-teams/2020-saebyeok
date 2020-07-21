915a91e867409e8fcf8a66e692e06e7ca38964aa
Andy H. Jung <commaniakr@gmail.com>
2020-07-21 16:10:14 +0900

    chore: Prettier와 ESLint의 룰 일치시킴

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -2,7 +2,12 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
       </v-col>
 
       <v-col class="mb-4">
@@ -11,10 +16,9 @@
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          <a href="https://community.vuetifyjs.com" target="_blank"
+            >Discord Community</a
+          >
         </p>
       </v-col>
 
@@ -28,7 +32,8 @@
             :href="next.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ next.text }}</a>
+            >{{ next.text }}</a
+          >
         </v-row>
       </v-col>
 
@@ -42,7 +47,8 @@
             :href="link.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ link.text }}</a>
+            >{{ link.text }}</a
+          >
         </v-row>
       </v-col>
 
@@ -56,7 +62,8 @@
             :href="eco.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ eco.text }}</a>
+            >{{ eco.text }}</a
+          >
         </v-row>
       </v-col>
     </v-row>
@@ -71,53 +78,54 @@ export default {
     ecosystem: [
       {
         text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
+        href: "https://github.com/vuetifyjs/vuetify-loader",
       },
       {
         text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
+        href: "https://github.com/vuetifyjs/vuetify",
       },
       {
         text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
     ],
     importantLinks: [
       {
         text: "Documentation",
-        href: "https://vuetifyjs.com"
+        href: "https://vuetifyjs.com",
       },
       {
         text: "Chat",
-        href: "https://community.vuetifyjs.com"
+        href: "https://community.vuetifyjs.com",
       },
       {
         text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
+        href: "https://madewithvuejs.com/vuetify",
       },
       {
         text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
+        href: "https://twitter.com/vuetifyjs",
       },
       {
         text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
+        href: "https://medium.com/vuetify",
+      },
     ],
     whatsNext: [
       {
         text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
+        href: "https://vuetifyjs.com/components/api-explorer",
       },
       {
         text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
       },
       {
         text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+        href:
+          "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
 };
 </script>
