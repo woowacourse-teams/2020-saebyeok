ef6b33b65c44b047b565eac51704f03afdca545b
Andy H. Jung <commaniakr@gmail.com>
2020-07-21 15:29:46 +0900

    chore: Vue.js 프론트엔드 프로젝트 설정

---
+++ src/frontend/vue.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  outputDir: path.resolve(__dirname, "../" + "main/resources/static"),
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+};
