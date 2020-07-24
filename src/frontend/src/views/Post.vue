0e8662523e643b12ca9c6d9d86f9445a283780c4
icyMojito <dev.icymojito@gmail.com>
2020-07-24 16:20:55 +0900

    feat: 대분류 이모지와 소분류 태그 1차 배치
    - 데모용으로 제작하였으므로 차후 배치 수정 필요

--- src/frontend/src/views/Post.vue
+++ src/frontend/src/views/Post.vue
@@ -1,34 +1,64 @@
 <template>
   <div>
-    <v-col cols="12" md="8">
-      <v-textarea
-        solo
-        no-resize
-        name="input-7-4"
-        label="당신의 마음을 들려주세요."
-        v-model="content"
-      ></v-textarea>
-      <v-checkbox
-        v-model="isCommentAllowed"
-        label="댓글을 허용할게요."
-      ></v-checkbox>
-      <v-btn
-        class="ma-2"
-        outlined
-        block
-        width="100%"
-        color="#df93bd"
-        @click="submit"
-        >남기기</v-btn
-      >
-    </v-col>
+    <v-container>
+      <v-layout column>
+        <v-row no-gutters>
+          <v-col
+            cols="2"
+            style="font-size: 60px; text-align: center; line-height: 60px"
+          >
+            😊
+          </v-col>
+          <v-col cols="10">
+            <v-chip-group
+              column
+              multiple
+              active-class="black--text text--accent-4"
+            >
+              <v-chip v-for="tag in tags" :key="tag">
+                {{ tag }}
+              </v-chip>
+            </v-chip-group>
+          </v-col>
+        </v-row>
+        <br />
+        <v-flex>
+          <v-textarea
+            solo
+            no-resize
+            rows="9"
+            name="input-7-4"
+            label="당신의 마음을 들려주세요."
+            v-model="content"
+          ></v-textarea>
+        </v-flex>
+        <v-flex>
+          <v-checkbox
+            v-model="isCommentAllowed"
+            label="댓글을 허용할게요."
+          ></v-checkbox>
+        </v-flex>
+        <v-flex>
+          <v-btn
+            class="ma-2"
+            depressed
+            block
+            dark
+            width="100%"
+            color="rgba(230, 197, 234)"
+            @click="submit"
+            >남기기</v-btn
+          >
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-import ApiService from '@/api/index.js';
+  import ApiService from '@/api/index.js';
 
-export default {
+  export default {
   components: {},
   props: {
     source: String
@@ -36,7 +66,16 @@ export default {
   data() {
     return {
       content: '',
-      isCommentAllowed: true
+      isCommentAllowed: true,
+      tags: [
+        '# 즐거워요',
+        '# 기뻐요',
+        '# 행복해요',
+        '# 재밌어요',
+        '# 만족스러워요',
+        '# 흥미진진해요',
+        '# 기대돼요'
+      ]
     };
   },
   methods: {
