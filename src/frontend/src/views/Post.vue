875548827b97ae6aa311acb9028e9d7016587682
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-07-24 13:27:14 +0900

    feat: Post.vue 초기 레이아웃 완성

--- src/frontend/src/views/Post.vue
+++ src/frontend/src/views/Post.vue
@@ -1,18 +1,54 @@
 <template>
   <div>
-    <editor></editor>
+    <v-col cols="12" md="8">
+      <v-textarea
+        solo
+        no-resize
+        name="input-7-4"
+        label="당신의 마음을 들려주세요."
+        v-model="content"
+      ></v-textarea>
+      <v-checkbox
+        v-model="isCommentAllowed"
+        label="댓글을 허용할게요."
+      ></v-checkbox>
+      <v-btn
+        class="ma-2"
+        outlined
+        block
+        width="100%"
+        color="#df93bd"
+        @click="submit"
+        >남기기</v-btn
+      >
+    </v-col>
   </div>
 </template>
 
 <script>
-import Editor from '@/components/Editor.vue';
+import ApiService from '@/api/index.js';
 
 export default {
-  components: {
-    Editor
-  },
+  components: {},
   props: {
     source: String
+  },
+  data() {
+    return {
+      content: '',
+      isCommentAllowed: true
+    };
+  },
+  methods: {
+    submit() {
+      const articleCreateRequest = {
+        content: this.content,
+        emotion: '기뻐요',
+        isCommentAllowed: this.isCommentAllowed
+      };
+      console.log(articleCreateRequest);
+      ApiService.post('/articles', articleCreateRequest);
+    }
   }
 };
 </script>
