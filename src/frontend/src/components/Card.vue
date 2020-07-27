9c33a547a3b68f89f8a76d562e6980dd3a8c3190
gunhee lee <dlrjsgml42@naver.com>
2020-07-27 15:15:43 +0900

    feat: Card 컴포넌트의 내용 변경, data.sql에 테스트 데이터 추가
    - data.sql에 길이가 긴 게시물, 댓글이 허용되지 않은 게시물 테스트 추가
    - Card 컴포넌트 내에 날짜 표시, 댓글, 공감수, 감정 태그를 추가. 아직 Article을 통해 전달받지 못하는 값은 임의로 계산함
    - 날짜의 경우, 1일 이내일 경우 몇 시간 전인지 반올림하여 계산하고, 이외에는 시간을 제외한 날짜만 출력하도록 함

--- src/frontend/src/components/Card.vue
+++ src/frontend/src/components/Card.vue
@@ -1,8 +1,33 @@
 <template>
   <v-card class="mx-auto" color="#faf9f5" max-width="400">
     <v-card-title>
-      <v-icon large left>mdi-twitter</v-icon>
-      <span class="title font-weight-light">기뻐요</span>
+      <v-row no-gutters>
+        <v-col
+          class="d-flex justify-end"
+          style="font-size: 15px; text-align: center; line-height: 15px"
+        >
+          {{ createdDate }}
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col
+          cols="2"
+          style="font-size: 60px; text-align: center; line-height: 60px"
+        >
+          {{ emotion }}
+        </v-col>
+        <v-col align="right" cols="10" justify="end">
+          <v-chip-group
+            active-class="black--text text--accent-4"
+            column
+            multiple
+          >
+            <v-chip :key="tag" v-for="tag in tags">
+              {{ tag }}
+            </v-chip>
+          </v-chip-group>
+        </v-col>
+      </v-row>
     </v-card-title>
 
     <v-card-text class="headline">
@@ -11,11 +36,20 @@
 
     <v-card-actions>
       <v-list-item class="grow">
-        <v-row align="center" justify="end">
-          <v-icon class="mr-1">mdi-hand-heart</v-icon>
-          <span class="subheading mr-2">256</span>
-          <v-icon class="mr-1">mdi-alarm-light</v-icon>
-          <span class="subheading">45</span>
+        <v-row>
+          <v-col align="left" cols="6" justify="end">
+            <v-icon class="mr-1">mdi-hand-heart</v-icon>
+            <span class="subheading mr-2">{{ recommand }}</span>
+          </v-col>
+          <v-col align="right" cols="4" justigy="end">
+            <div v-if="article.isCommentAllowed">
+              <v-icon class="mr-1">mdi-comment</v-icon>
+              <span class="subheading">{{ article.comments.length }}</span>
+            </div>
+          </v-col>
+          <v-col align="right" cols="2" justigy="end">
+            <v-icon class="mr-1">mdi-alarm-light</v-icon>
+          </v-col>
         </v-row>
       </v-list-item>
     </v-card-actions>
@@ -25,6 +59,33 @@
 <script>
 export default {
   name: 'Card',
+  data() {
+    return {
+      //아직 article에 없는 값을 임시로 설정
+      emotion: '😊',
+      tags: ['# 즐거워요', '# 기뻐요', '# 행복해요'],
+      recommand: 42
+    };
+  },
+  computed: {
+    createdDate: function() {
+      let date = new Date(this.article.createdDate);
+      let now = new Date();
+      let gap = (now.getTime() - date.getTime()) / 1000;
+      if (gap > 86400) {
+        return (
+          date.getFullYear() +
+          '년 ' +
+          (date.getMonth() + 1) +
+          '월 ' +
+          date.getDate() +
+          '일'
+        );
+      } else {
+        return Math.floor(gap / 3600) + '시간 전';
+      }
+    }
+  },
   props: {
     article: {
       type: Object,
