8f42d1a18bdc97b0d0aea4767af10292d56950fb
gunhee lee <dlrjsgml42@naver.com>
2020-07-27 15:20:54 +0900

    refactor: CardList.vue 내의 v-for 로직의 잘못된 키 값 수정

--- src/frontend/src/components/CardList.vue
+++ src/frontend/src/components/CardList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row dense>
-      <v-col :key="article.content" cols="12" v-for="article in articles">
+      <v-col :key="article.id" cols="12" v-for="article in articles">
         <card :article="article"></card>
       </v-col>
     </v-row>
@@ -9,16 +9,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import Card from '@/components/Card.vue';
+  import {mapGetters} from 'vuex';
+  import Card from '@/components/Card.vue';
 
-export default {
-  name: 'CardList',
-  components: {
-    Card
-  },
-  computed: {
-    ...mapGetters(['articles'])
-  }
-};
+  export default {
+    name: 'CardList',
+    components: {
+      Card
+    },
+    computed: {
+      ...mapGetters(['articles'])
+    }
+  };
 </script>
