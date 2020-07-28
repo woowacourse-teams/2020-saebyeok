903caa0ce9b2c77dfc4d3e5b6e4a4742661ca42d
icyMojito <dev.icymojito@gmail.com>
2020-07-28 13:23:27 +0900

    feat: 서비스 운영 시 log가 파일로 남도록 logback 설정 추가

---
+++ src/main/java/com/saebyeok/saebyeok/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.saebyeok.saebyeok.config;
+
+import com.saebyeok.saebyeok.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LogInterceptor logInterceptor;
+
+    public WebMvcConfig(LogInterceptor logInterceptor) {
+        this.logInterceptor = logInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/*");
+    }
+}
