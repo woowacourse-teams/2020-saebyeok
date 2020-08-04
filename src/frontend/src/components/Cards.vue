caffce713682d8dc5328e557e43f976a2fe0c104
gunhee lee <dlrjsgml42@naver.com>
2020-08-04 17:35:21 +0900

    feat: Cards.vue에 infinite scroll 기능 추가, vue-infinite-scroll 프레임워크 추가
    - 현재 axios를 써서 바로 api 동작을 수행하도록 Cards에 구현하였음. 이후 모듈화 작업 수행 예정

--- src/frontend/src/components/Cards.vue
+++ src/frontend/src/components/Cards.vue
@@ -5,20 +5,82 @@
         <card :article="article"></card>
       </v-col>
     </v-row>
+    <infinite-loading
+      v-if="articles.length"
+      @infinite="infiniteHandler"
+      force-use-infinite-wrapper="v-row"
+      spinner="waveDots"
+    >
+    </infinite-loading>
   </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import Card from '@/components/Card.vue';
+import axios from 'axios';
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'Cards',
-  components: {
-    Card
+  data() {
+    return {
+      articles: [],
+      page: 1
+    };
   },
-  computed: {
-    ...mapGetters(['articles'])
+  created() {
+    async function getArticlesFromApi() {
+      try {
+        const init = await axios.get(`/api/articles/page`, {
+          params: {
+            page: 0,
+            size: 5
+          }
+        });
+        // const data = await init.json();
+        // return data;
+        console.log('TWICE   ', init.data);
+        return init.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getArticlesFromApi().then(data => {
+      console.log('From...Api...!!' + data);
+      this.articles = data;
+    });
+  },
+  components: {
+    Card,
+    InfiniteLoading
+  },
+  methods: {
+    infiniteHandler($state) {
+      axios
+        .get(`/api/articles/page`, {
+          params: {
+            page: this.page,
+            size: 5
+          }
+        })
+        .then(({ data }) => {
+          console.log('OHMYGIRL ', data);
+          setTimeout(() => {
+            if (data.length) {
+              this.page++;
+              this.articles = this.articles.concat(data);
+              $state.loaded();
+              console.log('after : ', this.articles.length, this.page);
+            } else {
+              $state.complete();
+            }
+          }, 1000);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   }
 };
 </script>
