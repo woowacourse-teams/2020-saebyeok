0079564810dd466aacc25449fdfe659fca7b05e9
gunhee lee <dlrjsgml42@naver.com>
2020-08-05 13:29:06 +0900

    feat: data.sql 내용수정 및 Cards에 있던 infinite-loading 로직 Feed로 이동

--- src/frontend/src/components/Cards.vue
+++ src/frontend/src/components/Cards.vue
@@ -5,80 +5,21 @@
         <card :article="article"></card>
       </v-col>
     </v-row>
-    <infinite-loading
-      v-if="articles.length"
-      @infinite="infiniteHandler"
-      force-use-infinite-wrapper="v-row"
-      spinner="waveDots"
-    >
-    </infinite-loading>
   </v-container>
 </template>
 
 <script>
 import Card from '@/components/Card.vue';
-import axios from 'axios';
-import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'Cards',
-  data() {
-    return {
-      articles: [],
-      page: 1
-    };
-  },
-  created() {
-    async function getArticlesFromApi() {
-      try {
-        const init = await axios.get(`/api/articles`, {
-          params: {
-            page: 0,
-            size: 5
-          }
-        });
-        // const data = await init.json();
-        // return data;
-        console.log('TWICE   ', init.data);
-        return init.data;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getArticlesFromApi().then(data => {
-      this.articles = data;
-    });
-  },
   components: {
-    Card,
-    InfiniteLoading
+    Card
   },
-  methods: {
-    infiniteHandler($state) {
-      axios
-        .get(`/api/articles`, {
-          params: {
-            page: this.page,
-            size: 5
-          }
-        })
-        .then(({ data }) => {
-          console.log('OHMYGIRL ', data);
-          setTimeout(() => {
-            if (data.length) {
-              this.page++;
-              this.articles = this.articles.concat(data);
-              $state.loaded();
-              console.log('after : ', this.articles.length, this.page);
-            } else {
-              $state.complete();
-            }
-          }, 1000);
-        })
-        .catch(err => {
-          console.error(err);
-        });
+  props: {
+    articles: {
+      type: Object,
+      required: true
     }
   }
 };
--- src/frontend/src/views/Feed.vue
+++ src/frontend/src/views/Feed.vue
@@ -1,29 +1,90 @@
 <template>
   <div>
     <div class="mt-4 overflow-y-auto">
-      <cards />
+      <cards :articles="articles" />
     </div>
+    <infinite-loading
+      v-if="articles.length"
+      @infinite="infiniteHandler"
+      force-use-infinite-wrapper="cards"
+      spinner="waveDots"
+    >
+    </infinite-loading>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapGetters } from 'vuex';
 import Cards from '@/components/Cards.vue';
-import { FETCH_ARTICLES } from '@/store/shared/actionTypes';
+// import { FETCH_ARTICLES } from '@/store/shared/actionTypes';
+import axios from 'axios';
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'Feed',
+  data() {
+    return {
+      articles: [],
+      page: 1
+    };
+  },
   components: {
-    Cards
+    Cards,
+    InfiniteLoading
   },
   created() {
-    this.fetchArticles();
+    // this.fetchArticles();
+    async function getArticlesFromApi() {
+      try {
+        const init = await axios.get(`/api/articles`, {
+          params: {
+            page: 0,
+            size: 5
+          }
+        });
+        // const data = await init.json();
+        // return data;
+        console.log('TWICE   ', init.data);
+        return init.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getArticlesFromApi().then(data => {
+      this.articles = data;
+    });
   },
   computed: {
     ...mapGetters(['articles'])
   },
   methods: {
-    ...mapActions([FETCH_ARTICLES])
+    // ...mapActions([FETCH_ARTICLES])
+    infiniteHandler($state) {
+      axios
+        .get(`/api/articles`, {
+          params: {
+            page: this.page,
+            size: 5
+          }
+        })
+        .then(({ data }) => {
+          console.log('OHMYGIRL ', data);
+          setTimeout(() => {
+            if (data.length) {
+              this.page++;
+              this.articles = this.articles.concat(data);
+              $state.loaded();
+              console.log('after : ', this.articles.length, this.page);
+            } else {
+              $state.complete();
+            }
+          }, 1000);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   },
   props: {
     source: String
