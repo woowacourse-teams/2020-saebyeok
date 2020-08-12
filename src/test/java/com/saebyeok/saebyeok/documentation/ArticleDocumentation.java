fdb23d6d93d274fbda7d5f8af4f6ae0c8412e871
gunhee lee <dlrjsgml42@naver.com>
2020-08-12 17:17:40 +0900

    test: 문서화 테스트 수정 및 테스트 코드의 오류 수정
    - EmotionAcceptanceTest와 EmotionServiceTest의 커스텀 예외 클래스에 대한 오류 수정
    - ArticleDocumentation에 Article DTO 클래스에 대한 변경사항 반영

--- src/test/java/com/saebyeok/saebyeok/documentation/ArticleDocumentation.java
+++ src/test/java/com/saebyeok/saebyeok/documentation/ArticleDocumentation.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saebyeok.saebyeok.controller.ArticleController;
 import com.saebyeok.saebyeok.documentation.common.Documentation;
 import com.saebyeok.saebyeok.domain.Article;
-import com.saebyeok.saebyeok.dto.ArticleCreateRequest;
-import com.saebyeok.saebyeok.dto.ArticleResponse;
-import com.saebyeok.saebyeok.dto.CommentResponse;
-import com.saebyeok.saebyeok.dto.TokenResponse;
+import com.saebyeok.saebyeok.dto.*;
 import com.saebyeok.saebyeok.service.ArticleService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -60,7 +57,7 @@ public class ArticleDocumentation extends Documentation {
 
     @Test
     void createArticle() throws Exception {
-        ArticleCreateRequest articleCreateRequest = new ArticleCreateRequest("내용", "기뻐요", true);
+        ArticleCreateRequest articleCreateRequest = new ArticleCreateRequest("내용", 1L, Arrays.asList(1L, 2L), true);
         Article article = articleCreateRequest.toArticle();
 
         given(articleService.createArticle(any(), any())).willReturn(article);
@@ -68,136 +65,151 @@ public class ArticleDocumentation extends Documentation {
         String content = objectMapper.writeValueAsString(articleCreateRequest);
 
         this.mockMvc.perform(post("/api/articles").
-            header("Authorization", tokenResponse.getAccessToken()).
-            accept(MediaType.APPLICATION_JSON).
-            content(content).
-            contentType(MediaType.APPLICATION_JSON)).
-            andExpect(status().isCreated()).
-            andDo(print()).
-            andDo(document("articles/create",
-                           getDocumentRequest(),
-                           getDocumentResponse(),
-                           requestHeaders(
-                               headerWithName("Authorization").description("Bearer auth credentials")
-                           ),
-                           requestFields(
-                               fieldWithPath("content").type(JsonFieldType.STRING).description("게시물 내용"),
-                               fieldWithPath("emotion").type(JsonFieldType.STRING).description("게시물 감정"),
-                               fieldWithPath("isCommentAllowed").type(JsonFieldType.BOOLEAN).description("게시물의 댓글 허용 " +
-                                                                                                             "여부")),
-                           responseHeaders(
-                               headerWithName("Location").description("생성 성공 시 해당 주소로 이동")
-                           )
-            ));
+                header("Authorization", tokenResponse.getAccessToken()).
+                accept(MediaType.APPLICATION_JSON).
+                content(content).
+                contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().isCreated()).
+                andDo(print()).
+                andDo(document("articles/create",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer auth credentials")
+                        ),
+                        requestFields(
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("게시물 내용"),
+                                fieldWithPath("emotionId").type(JsonFieldType.NUMBER).description("게시물 감정 대분류의 ID"),
+                                fieldWithPath("subEmotionIds[]").type(JsonFieldType.ARRAY).description("게시물 감정 소분류의 ID 목록"),
+                                fieldWithPath("isCommentAllowed").type(JsonFieldType.BOOLEAN).description("게시물의 댓글 허용 " +
+                                        "여부")),
+                        responseHeaders(
+                                headerWithName("Location").description("생성 성공 시 해당 주소로 이동")
+                        )
+                ));
     }
 
     @Test
     void readArticle() throws Exception {
         List<CommentResponse> comments = Arrays.asList(new CommentResponse(1L, "댓글1", "닉네임1", false,
-                                                                           LocalDateTime.now()));
-        ArticleResponse articleResponse = new ArticleResponse(ARTICLE_ID, "내용", LocalDateTime.now(), "기뻐요", true,
-                                                              comments);
+                LocalDateTime.now()));
+        EmotionResponse emotionResponse = new EmotionResponse(1L, "기뻐요", "이미지 리소스 링크");
+        List<SubEmotionResponse> subEmotionResponses = Arrays.asList(new SubEmotionResponse(1L, "행복해요"), new SubEmotionResponse(2L, "설레요"));
+        ArticleResponse articleResponse = new ArticleResponse(ARTICLE_ID, "내용", LocalDateTime.now(), emotionResponse, subEmotionResponses, true, comments);
 
         given(articleService.readArticle(any())).willReturn(articleResponse);
 
         this.mockMvc.perform(get("/api/articles/{articleId}", ARTICLE_ID).
-            header("Authorization", tokenResponse.getAccessToken()).
-            accept(MediaType.APPLICATION_JSON)).
-            andExpect(status().isOk()).
-            andDo(print()).
-            andDo(document("articles/read",
-                           getDocumentRequest(),
-                           getDocumentResponse(),
-                           requestHeaders(
-                               headerWithName("Authorization").description("Bearer auth credentials")
-                           ),
-                           pathParameters(
-                               parameterWithName("articleId").description("조회할 게시물의 ID")
-                           ),
-                           responseFields(
-                               fieldWithPath("id").type(JsonFieldType.NUMBER).description("조회할 게시물의 ID"),
-                               fieldWithPath("content").type(JsonFieldType.STRING).description("조회할 게시물의 내용"),
-                               fieldWithPath("createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 작성 시간"),
-                               fieldWithPath("emotion").type(JsonFieldType.STRING).description("조회할 게시물의 감정"),
-                               fieldWithPath("isCommentAllowed").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글" +
-                                                                                                             " 허용 여부"),
-                               fieldWithPath("comments[]").type(JsonFieldType.ARRAY).description("조회할 게시물의 댓글 목록"),
-                               fieldWithPath("comments[].id").type(JsonFieldType.NUMBER).description("조회할 게시물의 댓글의 ID"),
-                               fieldWithPath("comments[].content").type(JsonFieldType.STRING).description("조회할 게시물의 " +
-                                                                                                              "댓글의 내용"),
-                               fieldWithPath("comments[].nickname").type(JsonFieldType.STRING).description("조회할 게시물의 " +
-                                                                                                               "댓글의 " +
-                                                                                                               "닉네임"),
-                               fieldWithPath("comments[].isDeleted").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글의 삭제 여부"),
-                               fieldWithPath("comments[].createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 댓글의 작성 시간")
-                           )
-            ));
+                header("Authorization", tokenResponse.getAccessToken()).
+                accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andDo(print()).
+                andDo(document("articles/read",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer auth credentials")
+                        ),
+                        pathParameters(
+                                parameterWithName("articleId").description("조회할 게시물의 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("조회할 게시물의 ID"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("조회할 게시물의 내용"),
+                                fieldWithPath("createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 작성 시간"),
+                                fieldWithPath("emotion").type(JsonFieldType.OBJECT).description("조회할 게시물의 감정 대분류"),
+                                fieldWithPath("emotion.id").type(JsonFieldType.NUMBER).description("조회할 게시물의 감정 대분류의 ID"),
+                                fieldWithPath("emotion.name").type(JsonFieldType.STRING).description("조회할 게시물의 감정 대분류의 이름"),
+                                fieldWithPath("emotion.imageResource").type(JsonFieldType.STRING).description("조회할 게시물의 감정 대분류의 이미지 리소스 링크"),
+                                fieldWithPath("subEmotions[]").type(JsonFieldType.ARRAY).description("조회할 게시물의 감정 소분류 목록"),
+                                fieldWithPath("subEmotions[].id").type(JsonFieldType.NUMBER).description("조회할 게시물의 감정 소분류의 ID"),
+                                fieldWithPath("subEmotions[].name").type(JsonFieldType.STRING).description("조회할 게시물의 감정 소분류의 이름"),
+                                fieldWithPath("isCommentAllowed").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글" +
+                                        " 허용 여부"),
+                                fieldWithPath("comments[]").type(JsonFieldType.ARRAY).description("조회할 게시물의 댓글 목록"),
+                                fieldWithPath("comments[].id").type(JsonFieldType.NUMBER).description("조회할 게시물의 댓글의 ID"),
+                                fieldWithPath("comments[].content").type(JsonFieldType.STRING).description("조회할 게시물의 " +
+                                        "댓글의 내용"),
+                                fieldWithPath("comments[].nickname").type(JsonFieldType.STRING).description("조회할 게시물의 " +
+                                        "댓글의 " +
+                                        "닉네임"),
+                                fieldWithPath("comments[].isDeleted").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글의 삭제 여부"),
+                                fieldWithPath("comments[].createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 댓글의 작성 시간")
+                        )
+                ));
     }
 
     @Test
     void getArticles() throws Exception {
         List<CommentResponse> comments = Arrays.asList(new CommentResponse(1L, "댓글1", "닉네임1", false,
-                                                                           LocalDateTime.now()));
-        ArticleResponse articleResponse = new ArticleResponse(ARTICLE_ID, "내용", LocalDateTime.now(), "기뻐요", true,
-                                                              comments);
+                LocalDateTime.now()));
+        EmotionResponse emotionResponse = new EmotionResponse(1L, "기뻐요", "이미지 리소스 링크");
+        List<SubEmotionResponse> subEmotionResponses = Arrays.asList(new SubEmotionResponse(1L, "행복해요"), new SubEmotionResponse(2L, "설레요"));
+        ArticleResponse articleResponse = new ArticleResponse(ARTICLE_ID, "내용", LocalDateTime.now(), emotionResponse, subEmotionResponses, true, comments);
 
         List<ArticleResponse> articleResponses = Arrays.asList(articleResponse);
 
         given(articleService.getArticles(anyInt(), anyInt())).willReturn(articleResponses);
 
         this.mockMvc.perform(get("/api/articles?page=0&size=5").
-            header("Authorization", tokenResponse.getAccessToken()).
-            accept(MediaType.APPLICATION_JSON)).
-            andExpect(status().isOk()).
-            andDo(print()).
-            andDo(document("articles/get",
-                           getDocumentRequest(),
-                           getDocumentResponse(),
-                           requestHeaders(
-                               headerWithName("Authorization").description("Bearer auth credentials")
-                           ),
-                           requestParameters(
-                               parameterWithName("page").description("확인할 게시물의 페이지"),
-                               parameterWithName("size").description("확인할 게시물의 개수")
-                           ),
-                           responseFields(
-                               fieldWithPath("[]").type(JsonFieldType.ARRAY).description("전체 게시물의 목록"),
-                               fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("게시물의 ID"),
-                               fieldWithPath("[].content").type(JsonFieldType.STRING).description("게시물의 내용"),
-                               fieldWithPath("[].createdDate").type(JsonFieldType.STRING).description("게시물의 작성 시간"),
-                               fieldWithPath("[].emotion").type(JsonFieldType.STRING).description("게시물의 감정"),
-                               fieldWithPath("[].isCommentAllowed").type(JsonFieldType.BOOLEAN).description("게시물의 댓글" +
-                                                                                                                " 허용 " +
-                                                                                                                "여부"),
-                               fieldWithPath("[].comments[]").type(JsonFieldType.ARRAY).description("조회할 게시물의 댓글 목록"),
-                               fieldWithPath("[].comments[].id").type(JsonFieldType.NUMBER).description("조회할 게시물의 댓글의" +
-                                                                                                            " ID"),
-                               fieldWithPath("[].comments[].content").type(JsonFieldType.STRING).description("조회할 게시물의 " +
-                                                                                                                 "댓글의 내용"),
-                               fieldWithPath("[].comments[].nickname").type(JsonFieldType.STRING).description("조회할 게시물의 " +
-                                                                                                                  "댓글의 " +
-                                                                                                                  "닉네임"),
-                               fieldWithPath("[].comments[].isDeleted").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글의 삭제 여부"),
-                               fieldWithPath("[].comments[].createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 댓글의 작성 시간")
-                           )
-            ));
+                header("Authorization", tokenResponse.getAccessToken()).
+                accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andDo(print()).
+                andDo(document("articles/get",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer auth credentials")
+                        ),
+                        requestParameters(
+                                parameterWithName("page").description("확인할 게시물의 페이지"),
+                                parameterWithName("size").description("확인할 게시물의 개수")
+                        ),
+                        responseFields(
+                                fieldWithPath("[]").type(JsonFieldType.ARRAY).description("전체 게시물의 목록"),
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("게시물의 ID"),
+                                fieldWithPath("[].content").type(JsonFieldType.STRING).description("게시물의 내용"),
+                                fieldWithPath("[].createdDate").type(JsonFieldType.STRING).description("게시물의 작성 시간"),
+                                fieldWithPath("[].emotion").type(JsonFieldType.OBJECT).description("게시물의 감정 대분류"),
+                                fieldWithPath("[].emotion.id").type(JsonFieldType.NUMBER).description("게시물의 감정 대분류의 ID"),
+                                fieldWithPath("[].emotion.name").type(JsonFieldType.STRING).description("게시물의 감정 대분류의 이름"),
+                                fieldWithPath("[].emotion.imageResource").type(JsonFieldType.STRING).description("게시물의 감정 대분류의 이미지 리소스 링크"),
+                                fieldWithPath("[].subEmotions[]").type(JsonFieldType.ARRAY).description("게시물의 감정 소분류 목록"),
+                                fieldWithPath("[].subEmotions[].id").type(JsonFieldType.NUMBER).description("게시물의 감정 소분류의 ID"),
+                                fieldWithPath("[].subEmotions[].name").type(JsonFieldType.STRING).description("게시물의 감정 소분류의 이름"),
+                                fieldWithPath("[].isCommentAllowed").type(JsonFieldType.BOOLEAN).description("게시물의 댓글" +
+                                        " 허용 " +
+                                        "여부"),
+                                fieldWithPath("[].comments[]").type(JsonFieldType.ARRAY).description("조회할 게시물의 댓글 목록"),
+                                fieldWithPath("[].comments[].id").type(JsonFieldType.NUMBER).description("조회할 게시물의 댓글의" +
+                                        " ID"),
+                                fieldWithPath("[].comments[].content").type(JsonFieldType.STRING).description("조회할 게시물의 " +
+                                        "댓글의 내용"),
+                                fieldWithPath("[].comments[].nickname").type(JsonFieldType.STRING).description("조회할 게시물의 " +
+                                        "댓글의 " +
+                                        "닉네임"),
+                                fieldWithPath("[].comments[].isDeleted").type(JsonFieldType.BOOLEAN).description("조회할 게시물의 댓글의 삭제 여부"),
+                                fieldWithPath("[].comments[].createdDate").type(JsonFieldType.STRING).description("조회할 게시물의 댓글의 작성 시간")
+                        )
+                ));
     }
 
     @Test
     void deleteArticle() throws Exception {
         this.mockMvc.perform(delete("/api/articles/{articleId}", ARTICLE_ID).
-            header("Authorization", tokenResponse.getAccessToken()).
-            accept(MediaType.APPLICATION_JSON)).
-            andExpect(status().isNoContent()).
-            andDo(print()).
-            andDo(document("articles/delete",
-                           getDocumentRequest(),
-                           getDocumentResponse(),
-                           requestHeaders(
-                               headerWithName("Authorization").description("Bearer auth credentials")),
-                           pathParameters(
-                               parameterWithName("articleId").description("삭제할 댓글의 게시물 ID")
-                           ))
-            );
+                header("Authorization", tokenResponse.getAccessToken()).
+                accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isNoContent()).
+                andDo(print()).
+                andDo(document("articles/delete",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer auth credentials")),
+                        pathParameters(
+                                parameterWithName("articleId").description("삭제할 댓글의 게시물 ID")
+                        ))
+                );
     }
 }
