063f27b36497160ea996a9ffd97f0dc6a6432659
chomily <sujinemilycho@gmail.com>
2020-08-13 10:04:49 +0900

    refactor: Member Entity 불필요한 생성자 삭제

--- src/main/java/com/saebyeok/saebyeok/domain/Member.java
+++ src/main/java/com/saebyeok/saebyeok/domain/Member.java
@@ -46,15 +46,6 @@ public class Member {
         return this.role.getKey();
     }
 
-    public Member(Long id, Integer birthYear, Gender gender, LocalDateTime createdDate, Boolean isDeleted, List<Article> articles) {
-        this.id = id;
-        this.birthYear = birthYear;
-        this.gender = gender;
-        this.createdDate = createdDate;
-        this.isDeleted = isDeleted;
-        this.articles = articles;
-    }
-
     public void addArticle(Article article) {
         // Todo: 편의 메소드 리팩토링(add를 중복으로 하는 경우 등)
         this.articles.add(article);
