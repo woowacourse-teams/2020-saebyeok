4a870f704d28201ac1439040d404d6d7979855e5
icyMojito <dev.icymojito@gmail.com>
2020-08-16 19:23:39 +0900

    feat: Member가 작성한 Article의 ID를 검색하는 기능을 ArticleRepository에 추가
    - 다이어리 분석 페이지에서 Emotion 별 Article 숫자를 가져올 때, Member가 작성한 Article의 숫자만 가져와야 하기 때문에 만듦.
      해당 기능으로 가져온 Article ID와 전체 Emotion ID를 통해 ArticleEmotion 테이블에서 Emotion 별 Article 숫자를 가져올 수 있음
    - List<Article>을 반환받는 것보다 NativeQuery로 List<Long> 값을 바로 조회하여 가져오는 것이 DB 성능에 유리
    - ArticleRepositoryTest에도 추가된 기능 테스트를 작성

--- src/main/java/com/saebyeok/saebyeok/domain/ArticleRepository.java
+++ src/main/java/com/saebyeok/saebyeok/domain/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.saebyeok.saebyeok.domain;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +13,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     public Optional<Article> findByIdAndCreatedDateGreaterThanEqual(Long id, LocalDateTime date);
 
     public List<Article> findAllByCreatedDateGreaterThanEqual(LocalDateTime date, Pageable pageable);
+
+    @Query(value = "select ARTICLE_ID from ARTICLE where MEMBER_ID = :memberId", nativeQuery = true)
+    public List<Long> findArticlesByMemberId(@Param("memberId") Long memberId);
 }
