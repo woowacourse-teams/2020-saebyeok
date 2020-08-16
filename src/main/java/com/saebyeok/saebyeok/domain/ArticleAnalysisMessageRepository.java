5194bcf70dfd393867de33b702cf8e68f7417780
icyMojito <dev.icymojito@gmail.com>
2020-08-17 03:56:09 +0900

    feat: ArticleAnalysisMessageRepository 생성

---
+++ src/main/java/com/saebyeok/saebyeok/domain/ArticleAnalysisMessageRepository.java
@@ -0,0 +1,6 @@
+package com.saebyeok.saebyeok.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleAnalysisMessageRepository extends JpaRepository<ArticleAnalysisMessage, Long> {
+}
