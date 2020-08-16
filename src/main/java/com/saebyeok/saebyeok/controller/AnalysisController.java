1022c6bb9eb321f7afd44c2978f1c6552e4350f7
icyMojito <dev.icymojito@gmail.com>
2020-08-17 04:23:59 +0900

    feat: AnalysisController에서 Member의 Article 분석 요청 처리 추가 완료
    - Emotion 별 Member가 작성한 Article의 개수, 최근 일정 기간 동안 Member가 작성한 Article의 Emotion 경향 분석 메시지를 응답으로 보냄

--- src/main/java/com/saebyeok/saebyeok/controller/AnalysisController.java
+++ src/main/java/com/saebyeok/saebyeok/controller/AnalysisController.java
@@ -5,6 +5,7 @@ import com.saebyeok.saebyeok.domain.MemberRepository;
 import com.saebyeok.saebyeok.dto.ArticlesAnalysisResponse;
 import com.saebyeok.saebyeok.exception.MemberNotFoundException;
 import com.saebyeok.saebyeok.security.User;
+import com.saebyeok.saebyeok.service.AnalysisService;
 import com.saebyeok.saebyeok.service.ArticleEmotionService;
 import com.saebyeok.saebyeok.service.ArticleService;
 import com.saebyeok.saebyeok.service.EmotionService;
@@ -15,16 +16,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
 public class AnalysisController {
+    private static final int INQUIRY_DAYS = 30;
+
     private final MemberRepository memberRepository;
     private final EmotionService emotionService;
     private final ArticleService articleService;
     private final ArticleEmotionService articleEmotionService;
+    private final AnalysisService analysisService;
 
     @GetMapping("/analysis/articles")
     public ResponseEntity<ArticlesAnalysisResponse> getArticleAnalysis(Authentication authentication) {
@@ -32,15 +37,29 @@ public class AnalysisController {
         User user = (User) authentication.getPrincipal();
         Member member = memberRepository.findById(user.getId())
                 .orElseThrow(() -> new MemberNotFoundException(user.getId()));
-
-        List<Long> memberArticlesIds = articleService.getMemberArticlesIds(member);
         List<Long> allEmotionsIds = emotionService.getAllEmotionsIds();
 
-        long[] articleEmotionsCount = articleEmotionService.getArticleEmotionsCount(memberArticlesIds,
-                                                                                    allEmotionsIds);
-
-        ArticlesAnalysisResponse articlesAnalysisResponse = new ArticlesAnalysisResponse(articleEmotionsCount);
-
+        long[] articleEmotionsCount = createArticleEmotionsCount(member, allEmotionsIds);
+        String articlesAnalysisMessage = createArticlesAnalysisMessage(member, allEmotionsIds);
+        ArticlesAnalysisResponse articlesAnalysisResponse = new ArticlesAnalysisResponse(articleEmotionsCount,
+                                                                                         articlesAnalysisMessage);
         return ResponseEntity.ok(articlesAnalysisResponse);
+    }
+
+    private long[] createArticleEmotionsCount(Member member, List<Long> allEmotionsIds) {
+        List<Long> memberArticlesIds = articleService.getMemberArticlesIds(member);
+
+        return articleEmotionService.getArticleEmotionsCount(memberArticlesIds, allEmotionsIds);
+    }
+
+    private String createArticlesAnalysisMessage(Member member, List<Long> allEmotionsIds) {
+        LocalDateTime cutDate = LocalDateTime.now().minusDays(INQUIRY_DAYS);
+
+        List<Long> articlesIdsByMemberIdAndCutDate =
+                articleService.getMemberArticlesIdsByCutDate(member, cutDate);
+        long mostArticleEmotionId = articleEmotionService.getMostEmotionIdInArticles(articlesIdsByMemberIdAndCutDate,
+                                                                                     allEmotionsIds);
+
+        return analysisService.getArticlesAnalysisMessage(mostArticleEmotionId);
     }
 }
