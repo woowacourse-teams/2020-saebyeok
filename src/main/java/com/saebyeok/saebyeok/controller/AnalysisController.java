a3d19f5338dcb1ecf3aca3b6aa7fc43e434ddaf7
icyMojito <dev.icymojito@gmail.com>
2020-08-17 16:16:09 +0900

    feat: AnalysisController에서 Member의 Comment 분석 요청 처리 추가 완료
    - Member가 작성한 Comment 총 개수를 응답으로 보냄
    - 차후 추천 도메인이 구현되면 추천받은 댓글의 수, 그에 따른 분석 메시지도 응답으로 보낼 예정

--- src/main/java/com/saebyeok/saebyeok/controller/AnalysisController.java
+++ src/main/java/com/saebyeok/saebyeok/controller/AnalysisController.java
@@ -3,12 +3,10 @@ package com.saebyeok.saebyeok.controller;
 import com.saebyeok.saebyeok.domain.Member;
 import com.saebyeok.saebyeok.domain.MemberRepository;
 import com.saebyeok.saebyeok.dto.ArticlesAnalysisResponse;
+import com.saebyeok.saebyeok.dto.CommentsAnalysisResponse;
 import com.saebyeok.saebyeok.exception.MemberNotFoundException;
 import com.saebyeok.saebyeok.security.User;
-import com.saebyeok.saebyeok.service.AnalysisService;
-import com.saebyeok.saebyeok.service.ArticleEmotionService;
-import com.saebyeok.saebyeok.service.ArticleService;
-import com.saebyeok.saebyeok.service.EmotionService;
+import com.saebyeok.saebyeok.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -29,10 +27,11 @@ public class AnalysisController {
     private final EmotionService emotionService;
     private final ArticleService articleService;
     private final ArticleEmotionService articleEmotionService;
+    private final CommentService commentService;
     private final AnalysisService analysisService;
 
     @GetMapping("/analysis/articles")
-    public ResponseEntity<ArticlesAnalysisResponse> getArticleAnalysis(Authentication authentication) {
+    public ResponseEntity<ArticlesAnalysisResponse> getArticlesAnalysis(Authentication authentication) {
         // TODO: 2020/08/16 커스텀 어노테이션으로 리팩토링
         User user = (User) authentication.getPrincipal();
         Member member = memberRepository.findById(user.getId())
@@ -61,5 +60,20 @@ public class AnalysisController {
                                                                                      allEmotionsIds);
 
         return analysisService.getArticlesAnalysisMessage(mostArticleEmotionId);
+    }
+
+    @GetMapping("/analysis/comments")
+    public ResponseEntity<CommentsAnalysisResponse> getCommentsAnalysis(Authentication authentication) {
+        // TODO: 2020/08/16 커스텀 어노테이션으로 리팩토링
+        User user = (User) authentication.getPrincipal();
+        Member member = memberRepository.findById(user.getId())
+                .orElseThrow(() -> new MemberNotFoundException(user.getId()));
+
+        Long totalCommentsCount = commentService.findTotalCommentsCountByMember(member);
+
+        // TODO: 2020/08/17 차후 추천받은 댓글의 수와, 그에 따른 메세지도 같이 전달?
+        CommentsAnalysisResponse commentsAnalysisResponse = new CommentsAnalysisResponse(totalCommentsCount);
+
+        return ResponseEntity.ok(commentsAnalysisResponse);
     }
 }
