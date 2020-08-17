c6d2a70613d59e1e0b5deed5e0cc245fcf404fb3
icyMojito <dev.icymojito@gmail.com>
2020-08-17 15:36:32 +0900

    feat: Member가 작성한 Comment의 총 개수를 계산하는 기능을 CommentRepository에 추가
    - 다이어리 분석 페이지에서 Member가 작성한 Comment의 총 개수를 보여줘야 하기 때문에 만듦
    - CommentRepositoryTest에도 추가된 기능 테스트를 작성

--- src/main/java/com/saebyeok/saebyeok/domain/CommentRepository.java
+++ src/main/java/com/saebyeok/saebyeok/domain/CommentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    public Long countCommentsByMember(Member member);
 }
