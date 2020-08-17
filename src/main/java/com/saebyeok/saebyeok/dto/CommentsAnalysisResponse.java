76906df12f9d5562b1f2f36d791e1863080cf741
icyMojito <dev.icymojito@gmail.com>
2020-08-17 16:05:47 +0900

    feat: Member가 작성한 Comment 분석 데이터를 담을 DTO인 CommentsAnalysisResponse 생성

---
+++ src/main/java/com/saebyeok/saebyeok/dto/CommentsAnalysisResponse.java
@@ -0,0 +1,12 @@
+package com.saebyeok.saebyeok.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CommentsAnalysisResponse {
+    private long totalCommentsCount;
+}
