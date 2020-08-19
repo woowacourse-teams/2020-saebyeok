c73db9bbae52ef95c44b859db8296f4f4e326703
chomily <sujinemilycho@gmail.com>
2020-08-19 12:29:03 +0900

    feat: 공감 기능을 위한 ArticleLike, CommentLike Entity 생성

---
+++ src/main/java/com/saebyeok/saebyeok/domain/ArticleLike.java
@@ -0,0 +1,29 @@
+package com.saebyeok.saebyeok.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ArticleLike {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private Article article;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+}
