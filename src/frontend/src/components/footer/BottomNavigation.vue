85614c227103e6883d74e7cf15a61ee8c78675ec
Andy H. Jung <commaniakr@gmail.com>
2020-08-23 00:33:52 +0900

    feat: 아이폰 일부 모델에서 내비게이션 바에 하단 메뉴가 가려지는 현상 수정

--- src/frontend/src/components/footer/BottomNavigation.vue
+++ src/frontend/src/components/footer/BottomNavigation.vue
@@ -1,26 +1,58 @@
 <template>
-  <v-bottom-navigation app color="#B2A4D4" shift>
-    <v-btn to="/feed" height="100%">
-      <span>피드</span>
-      <v-icon large>mdi-rss</v-icon>
-    </v-btn>
+  <div>
+    <v-bottom-navigation
+      app
+      color="#B2A4D4"
+      shift
+      :class="{ iphoneX: isiPhoneX }"
+    >
+      <v-btn to="/feed" height="100%">
+        <span>피드</span>
+        <v-icon large>mdi-rss</v-icon>
+      </v-btn>
 
-    <v-btn to="/emotions" height="100%">
-      <span>글쓰기</span>
-      <v-icon large>mdi-pencil</v-icon>
-    </v-btn>
+      <v-btn to="/emotions" height="100%">
+        <span>글쓰기</span>
+        <v-icon large>mdi-pencil</v-icon>
+      </v-btn>
 
-    <v-btn to="/my-page" height="100%">
-      <span>다이어리</span>
-      <v-icon large>mdi-note</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+      <v-btn to="/my-page" height="100%">
+        <span>다이어리</span>
+        <v-icon large>mdi-note</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'bottom-navigation'
+  name: 'bottom-navigation',
+  data() {
+    return {
+      isiPhoneX: false
+    };
+  },
+  mounted() {
+    this.checkUserAgent();
+  },
+  methods: {
+    checkUserAgent() {
+      const userAgent = window.navigator.userAgent;
+      console.log(screen.width);
+      if (
+        userAgent.search('iPhone') > 0 &&
+        (screen.width === 375 || screen.width === 414)
+      ) {
+        this.isiPhoneX = true;
+      }
+    }
+  }
 };
 </script>
 
-<style></style>
+<style>
+.iphoneX {
+  height: 76px !important;
+  padding-bottom: 20px;
+}
+</style>
