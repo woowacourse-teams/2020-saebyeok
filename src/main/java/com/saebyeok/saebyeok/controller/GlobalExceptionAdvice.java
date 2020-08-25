56a1df05b1af1ff2f34a5fa63077371aec77f63d
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-08-25 23:21:34 +0900

    refactor: 더이상 필요 없는 커스텀 예외 제거

--- src/main/java/com/saebyeok/saebyeok/controller/GlobalExceptionAdvice.java
+++ src/main/java/com/saebyeok/saebyeok/controller/GlobalExceptionAdvice.java
@@ -17,7 +17,6 @@ public class GlobalExceptionAdvice {
     // TODO: 2020/07/20 exception을 어떻게 응답 처리할 것인지 방법을 고민해보자
 
     @ExceptionHandler({
-            InvalidLengthCommentException.class,
             CommentNotFoundException.class,
             ArticleNotFoundException.class,
             EmotionNotFoundException.class,
