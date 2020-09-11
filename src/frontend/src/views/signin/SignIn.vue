6ad6910434a0412b47dd273baf6243178a0a6711
Andy H. Jung <commaniakr@gmail.com>
2020-09-11 17:09:47 +0900

    refactor: 구글 로그인 프론트엔드 설정
    - 구글 로그인 버튼 추가
    - 자잘한 CSS 수정

--- src/frontend/src/views/signin/SignIn.vue
+++ src/frontend/src/views/signin/SignIn.vue
@@ -7,9 +7,17 @@
       <v-flex>
         <a href="/oauth2/authorization/naver">
           <v-img
+            class="naver-button"
             alt="네이버 아이디로 로그인"
-            width="200px"
+            width="194px"
             src="/img/naverLogin.png"
+        /></a>
+        <a href="/oauth2/authorization/google">
+          <v-img
+            class="google-button"
+            alt="Google로 로그인"
+            width="198px"
+            src="/img/googleLogin.png"
         /></a>
       </v-flex>
     </v-layout>
@@ -29,5 +37,11 @@ export default {};
 p {
   font-size: 40px;
   color: white;
+}
+.naver-button {
+  margin: 3px;
+}
+.google-button {
+  margin: 1px;
 }
 </style>
