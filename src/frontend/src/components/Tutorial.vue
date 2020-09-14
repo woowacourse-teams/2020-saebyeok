af7fb1ced34598fa6e06f3abe097ac136b8d76bc
icyMojito <dev.icymojito@gmail.com>
2020-09-14 14:25:56 +0900

    feat: 튜토리얼 팝업을 관리하는 컴포넌트 생성

---
+++ src/frontend/src/components/Tutorial.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="400">
+      <v-card>
+        <v-carousel hide-delimiters height="auto">
+          <v-carousel-item
+            v-for="(item, i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          ></v-carousel-item>
+        </v-carousel>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="purple darken-5" text @click="closeTutorial()"
+            >시작해볼게요</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ['dialog'],
+  data() {
+    return {
+      items: [
+        {
+          src: '/img/tutorial_1.png'
+        },
+        {
+          src: '/img/tutorial_2.png'
+        },
+        {
+          src: '/img/tutorial_3.png'
+        },
+        {
+          src: '/img/tutorial_4.png'
+        },
+        {
+          src: '/img/tutorial_5.png'
+        },
+        {
+          src: '/img/tutorial_6.png'
+        },
+        {
+          src: '/img/tutorial_7.png'
+        }
+      ]
+    };
+  },
+  methods: {
+    closeTutorial() {
+      localStorage.setItem('tutorial_check', 'yes');
+      this.dialog = false;
+    }
+  }
+};
+</script>
