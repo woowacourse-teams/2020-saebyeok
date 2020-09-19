84242095cdbdc515cbefd5b0e4585238c6710054
Andy H. Jung <commaniakr@gmail.com>
2020-09-19 18:48:30 +0900

    feat: 다이어리의 우측 상단 버튼을 Setting으로 변경

---
+++ src/frontend/src/components/SettingButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-icon @click.stop="dialog = true">mdi-settings</v-icon>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title class="text-h6">정말 로그아웃 하시겠어요?</v-card-title>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="#B2A4D4" text @click="dialog = false">아니요</v-btn>
+
+          <v-btn color="#B2A4D4" text @click="logout">네, 할게요</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+import { SHOW_SNACKBAR } from '../store/shared/mutationTypes';
+
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    ...mapMutations([SHOW_SNACKBAR]),
+    logout() {
+      localStorage.clear();
+      this.showSnackbar('성공적으로 로그아웃되었어요. 안녕히 가세요👋');
+      this.$router.replace({ name: 'SignIn' });
+    }
+  }
+};
+</script>
+
+<style></style>
