c0b01cee6102bf3e3db88235edf171ea34b8c0a6
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-09-23 00:58:36 +0900

    refactor: Recomment관련 파일 모두 제거

--- src/main/java/com/saebyeok/saebyeok/domain/RecommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saebyeok.saebyeok.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommentRepository extends JpaRepository<Recomment, Long> {
-
-}
