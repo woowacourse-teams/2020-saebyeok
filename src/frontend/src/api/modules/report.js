61d04fbc53cb9a4bdaa0cf204caa1ca72dac0174
gunhee lee <dlrjsgml42@naver.com>
2020-10-05 17:20:07 +0900

    feat: 신고 API 수정사항 adoc 및 frontend api에 반영

--- src/frontend/src/api/modules/report.js
+++ src/frontend/src/api/modules/report.js
@@ -9,26 +9,8 @@ const ReportService = {
   createArticleReport(report) {
     return ApiService.post(`${BASE_URL}/article`, report);
   },
-  getArticleReport(reportId) {
-    return ApiService.get(`${BASE_URL}/artcile/${reportId}`);
-  },
-  getArticleReports() {
-    return ApiService.get(`${BASE_URL}/article`);
-  },
-  deleteArticleReport(reportId) {
-    return ApiService.delete(`${BASE_URL}/article/${reportId}`);
-  },
   createCommentReport(report) {
     return ApiService.post(`${BASE_URL}/comment`, report);
-  },
-  getCommentReport(reportId) {
-    return ApiService.get(`${BASE_URL}/comment/${reportId}`);
-  },
-  getCommentReports() {
-    return ApiService.get(`${BASE_URL}/comment`);
-  },
-  deleteCommentReport(reportId) {
-    return ApiService.delete(`${BASE_URL}/comment/${reportId}`);
   }
 };
 
