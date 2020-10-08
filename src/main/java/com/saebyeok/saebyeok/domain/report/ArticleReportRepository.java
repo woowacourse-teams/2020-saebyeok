b068b7c9797c1887b7d80af8f74a60af225baf62
gunhee lee <dlrjsgml42@naver.com>
2020-10-08 13:01:56 +0900

    refactor: Report 도메인 설계에 맞춰 통합 진행, 불필요한 ArticleReport, CommentReport 삭제
    - ArticleReport, CommentReport 삭제
    - ArticleReport, CommentReport의 Repository 및 테스트 코드 삭제

--- src/main/java/com/saebyeok/saebyeok/domain/report/ArticleReportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.saebyeok.saebyeok.domain.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleReportRepository extends JpaRepository<ArticleReport, Long> {
-}
--- src/main/java/com/saebyeok/saebyeok/domain/report/CommentReportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.saebyeok.saebyeok.domain.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentReportRepository extends JpaRepository<CommentReport, Long> {
-}
