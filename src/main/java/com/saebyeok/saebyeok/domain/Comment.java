cac16edbc485f349eb7994493d1bd4af2bb137ab
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-10-26 14:55:24 +0900

    refactor: 리팩토링
    - 불필요한 import문 제거
    - 테스트 접근제어자 제거
    - Comment에 equals와 hashcode 오버라이드

--- src/main/java/com/saebyeok/saebyeok/domain/Comment.java
+++ src/main/java/com/saebyeok/saebyeok/domain/Comment.java
@@ -85,4 +85,17 @@ public class Comment implements Comparable<Comment> {
         }
         return this.createdDate.compareTo(other.createdDate);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/saebyeok/saebyeok/service/AnalysisService.java
+++ src/main/java/com/saebyeok/saebyeok/service/AnalysisService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
